1a8dbdbd363fbe61bda4ba0e2a7ea973066b740c
Tim Lucas <timcdlucas@gmail.com>
2022-01-06 11:09:46 +0000

    First full model with MLE.

--- R/agouti_fit.R
+++ R/agouti_fit.R
@@ -1,60 +1,89 @@
 
 
 
-agouti <- function(formula, data, 
-                   ID, inner_link, 
+agouti <- function(formula, data,
+                   ID, inner_link,
                    outer_link,
                    family, weights, ...){
-    
-    
-    if(family$family == 'gaussian'){
-        densf <- dnorm
-    } else if(family$family == 'poisson'){
-        densf <- dpois
-    } else if(family$family == 'binomial'){
-        densf <- dbinom
-    }
-    
 
-    
+  
+  # Easy setup for now.
+  set.seed(1)
+  N1 <- 50
+  N2 <- 10
+  ncovs <- 2
+  
+  beta <- c(0.1, -0.4)
+  x <- matrix(rnorm(N1 * ncovs), ncol = 2)
+  yy <- rnorm(N1)
+  ID <- sample(letters[seq(N2)], N1, replace = TRUE)
+  weights <- runif(N1)
+  inner <- function(x) x
+  outer <- function(x) x
+  
+  likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
+
+  
+  ll <- function(beta) objective(beta, x, yy, ID, inner, outer, likelihood_function, weights)
+  
+  fit <- maxLik::maxLik(ll, start = rep(0, length(beta)))
+  
+  est <- fit$estimate
+  
+  se <- sqrt(diag(solve(-fit$hessian)))
+  
+  CI <- rbind(est - 1.96 * se, 
+              est + 1.96 * se)
+  
+  out <- list(coefficients = est,
+              standard_errors = se,
+              ConfInt = CI)
+  return(out)
+  
 }
 
 
-nll <- function(beta, x, y, xID, yID, inner, 
-                outer, family, weights){
-    
-    linear_predictor <- t(beta %*% x)
-    
+
+
+nll <- function(beta, x, y, ID, inner,
+                outer, likelihood_function, weights){
+
+    linear_predictor <- (x %*% beta)[, 1]
+
     summand <- inner(linear_predictor) * weights
-    
-    d <- data.frame(xID, summand)
+
+    d <- data.frame(ID, summand)
     dsum <-
-        d %>% 
-        group_by(xID) %>%
+        d %>%
+        group_by(ID) %>%
         summarise(summed = sum(summand),
                   yhat = outer(summed))
-    
-    ydf <- data.frame(y, yID)
-    
-    dsum <- 
-        dsum %>%
-            left_join(ydf, by = c('xID' = 'yID')) %>%
-            mutate(nll = family(yhat, y, theta))
 
-  return(dsum}
+    ydf <- data.frame(y, ID)
+
+    
+    dsum <-
+        dsum %>%
+            left_join(ydf, by = c('ID' = 'ID')) %>%
+            mutate(nll = likelihood_function(yhat, y, theta))
+
+  return(dsum)
 }
 
 
-objective <- function(beta, x, y, xID, yID, inner, outer, family, weights){
 
-   dsum <- nll(beta, x, y, xID, yID, inner, outer, family, weights)
+
+
+objective <- function(beta, x, y, ID, inner, outer, likelihood_function, weights){
+
+   dsum <- nll(beta, x, y, ID, inner, outer, likelihood_function, weights)
 
   nll <- sum(dsum$nll)
  return(nll)
 }
 
 
-  
+
 
 
 
--- tests/testthat/test-agouti_fit.R
+++ tests/testthat/test-agouti_fit.R
@@ -1,77 +1,76 @@
-context('Basic tests that autoplot produces plots without errors')
+context('Test the model fitting functions fully')
 
-test_that('autoplot does not fail when it should not.', {
+test_that('nll works with Gaussian family.', {
   
-  skip_if_not_installed('INLA')
-  library(INLA)
-  data(Epil)
-  ##Define the model
-  formula = y ~ Trt + Age + V4 +
-           f(Ind, model="iid") + f(rand,model="iid")
-  result = inla(formula, family="poisson", data = Epil, control.predictor = list(compute = TRUE))
+  # nll <- function(beta, x, y, ID, inner,
+  #                outer, likelihood_function, weights){
+  set.seed(1)
+  N1 <- 50
+  N2 <- 10
+  ncovs <- 2
+    
+  beta <- c(0.1, -0.4)
+  x <- matrix(rnorm(N1 * ncovs), ncol = 2)
+  yy <- rnorm(N1)
+  ID <- sample(letters[seq(N2)], N1, replace = TRUE)
+  weights <- runif(N1)
+  inner <- function(x) x
+  outer <- function(x) x
 
-  # Test that figure builds
-  expect_error(p <- autoplot(result, which = 1:5), NA)
-  expect_error(p, NA)
+  likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
 
-  expect_error(autoplot(result, which = 1:4, CI = TRUE), NA)
-  expect_error(autoplot(result, which = 1:4, priors = TRUE), NA)
-  expect_error(autoplot(result, which = 1, priors = TRUE, CI = TRUE), NA)
+  out <- nll(beta, x, yy, ID, inner, outer, likelihood_function, weights)
+
+  expect_true(all(!(is.na(out$nll))))
+  expect_true(all(!(is.nan(out$nll))))
   
-  # test theme is modifiable
-  expect_error(p2 <- p + theme_bw(), NA)
-
-  expect_error(p2, NA)
-  expect_false(identical(p, p2))
-
-  # Test that you can change a single figure
-  p3 <- p
-  expect_error(p3[[2]] <- p3[[2]] + ggtitle('Hyper parameters'), NA)
-  expect_false(identical(p, p3))
-
 
 })
 
 
 
-test_that('Check which arg more carefully.', {
+test_that('nll works with 1 covariate.', {
   
-  skip_if_not_installed('INLA')
+  beta <- 2
+    
+    
+    
+    
+})
+
+
+test_that('Objective function works', {
   
-  data <- data.frame(y = rpois(100, 10), x1 = rnorm(100))
+  set.seed(1)
+  N1 <- 50
+  N2 <- 10
+  ncovs <- 2
   
-  data$x2 <- sin(data$y / 2) + rnorm(100, sd = 0.1)
+  beta <- c(0.1, -0.4)
+  x <- matrix(rnorm(N1 * ncovs), ncol = 2)
+  yy <- rnorm(N1)
+  ID <- sample(letters[seq(N2)], N1, replace = TRUE)
+  weights <- runif(N1)
+  inner <- function(x) x
+  outer <- function(x) x
   
-  #ggplot(data, aes(y, x2)) + geom_point()
+  likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
+  nll_val <- objective(beta, x, yy, ID, inner, outer, likelihood_function, weights)
   
-  model <- inla(y ~ x1 + x2, data = data, family = 'poisson')
   
-  expect_error(suppressWarnings(autoplot(model, which = c(1, 2))), NA)
+  expect_true(length(nll_val) == 1)
+  expect_true(!is.na(nll_val))
+  expect_true(is.numeric(nll_val))
+  
   
 })
 
 
-
-test_that('Plot prior different cases', {
-
-  skip_if_not_installed('INLA')
-  
-  library(sp)
-  data(meuse)
-  
-  meuse <- cbind(meuse, y.intercept = 1)
-  
-  modform <- cadmium ~ -1 + y.intercept + elev + dist + om
-  
-  meuse_model <- inla(modform, data = meuse)
-  
-  expect_error(autoplot(meuse_model, priors = TRUE), NA)
+test_that('We can optimise a model', {
   
   
-  modform2 <- cadmium ~ elev + dist + om
-  
-  meuse_model2 <- inla(modform2, data = meuse)
-  expect_error(autoplot(meuse_model2, priors = TRUE), NA)
-  
+  out <- agouti()
   
 })
+
+
