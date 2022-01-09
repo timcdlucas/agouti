ef4144cd4a747b53001e7d9a3256f195f84f3e2b
Tim Lucas <timcdlucas@gmail.com>
2022-01-09 23:27:01 +0000

    First full working model function.

--- R/agouti_fit.R
+++ R/agouti_fit.R
@@ -1,4 +1,22 @@
 
+# 
+# ff <- function(formula, data,
+#                      ID, inner_link,
+#                      outer_link,
+#                      family, weights){
+# 
+#   m <- match.call()
+#    mf <- match.call(expand.dots = FALSE)
+#   return(list(m, mf))
+# }
+# 
+# call <- ff(formula = form, d,
+#         ID = ID, inner,
+#         outer,
+#         family, weights)
+# m <- call[[1]]
+# mf <- call[[2]]
+
 
 
 agouti <- function(formula, data,
@@ -6,41 +24,32 @@ agouti <- function(formula, data,
                    outer_link,
                    family, weights, ...){
 
+  
 # mostly stolen from glm as I want it to be consistent.
-call <- match.call() ## family if(is.character(family)) family <- get(family, mode = "function", envir = parent.frame()) if(is.function(family)) family <- family() if(is.null(family$family)) {	print(family)	stop("'family' not recognized") }
+if(is.character(family)) family <- get(family, mode = "function", envir = parent.frame()) 
+if(is.function(family)) family <- family() 
+if(is.null(family$family)) {	
+  print(family)	
+  stop("'family' not recognized") 
+}
 
-  
+
+if(!is.null(weights) && !is.numeric(weights)) stop("'weights' must be a numeric vector") ## check weights and offset 
+if( !is.null(weights) && any(weights < 0) )	stop("negative weights not allowed")
+
+mf <- model.frame(formula = formula, data = data,
+                  subset = NULL, weights = weights)
+
+
+X <- model.matrix(formula, mf)
 Y <- model.response(mf, "any") # e.g. factors are allowed ## avoid problems with 1D arrays, but keep names
-
-## avoid any problems with 1D or nx1 arrays by as.vector. weights <- as.vector(model.weights(mf)) if(!is.null(weights) && !is.numeric(weights)) stop("'weights' must be a numeric vector") ## check weights and offset if( !is.null(weights) && any(weights < 0) )	stop("negative weights not allowed")
-
-mf <- match.call(expand.dots = FALSE) m <- match(c("formula", "data", "subset", "weights", "na.action", "etastart", "mustart", "offset"), names(mf), 0L) mf <- mf[c(1L, m)] mf$drop.unused.levels <- TRUE ## need stats:: for non-standard evaluation mf[[1L]] <- quote(stats::model.frame) mf <- eval(mf, parent.frame())
-
-mt <- attr(mf, "terms") # allow model.frame to have updated it
-
-
-X <- model.matrix(mt, mf, contrasts)
-
-
-
   
-  # Easy setup for now.
-  set.seed(1)
-  N1 <- 50
-  N2 <- 10
-  ncovs <- 2
-  
-  beta <- c(0.1, -0.4)
-  x <- matrix(rnorm(N1 * ncovs), ncol = 2)
-  yy <- rnorm(N1)
-  ID <- sample(letters[seq(N2)], N1, replace = TRUE)
-  weights <- runif(N1)
-  inner <- function(x) x
-  outer <- function(x) x
-  
-  likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
+  if(family$family == 'Gaussian'){
+    likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
+  }
+  if(inner_link == 'identity') inner <- function(x) x
+  if(outer_link == 'identity') outer <- function(x) x
 
-  
   ll <- function(beta) objective(beta, x, yy, ID, inner, outer, likelihood_function, weights)
   
   fit <- maxLik::maxLik(ll, start = rep(0, length(beta)))
@@ -55,6 +64,7 @@ X <- model.matrix(mt, mf, contrasts)
   out <- list(coefficients = est,
               standard_errors = se,
               ConfInt = CI)
+  class(out) <- 'agouti'
   return(out)
   
 }
