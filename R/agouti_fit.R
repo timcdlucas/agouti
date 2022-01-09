79ba73c901be2147d77203f55c114e19962594f8
Tim Lucas <timcdlucas@gmail.com>
2022-01-09 22:47:42 +0000

    Start copying over some checks and organisation from glm.

--- R/agouti_fit.R
+++ R/agouti_fit.R
@@ -6,6 +6,23 @@ agouti <- function(formula, data,
                    outer_link,
                    family, weights, ...){
 
+# mostly stolen from glm as I want it to be consistent.
+call <- match.call() ## family if(is.character(family)) family <- get(family, mode = "function", envir = parent.frame()) if(is.function(family)) family <- family() if(is.null(family$family)) {	print(family)	stop("'family' not recognized") }
+
+  
+Y <- model.response(mf, "any") # e.g. factors are allowed ## avoid problems with 1D arrays, but keep names
+
+## avoid any problems with 1D or nx1 arrays by as.vector. weights <- as.vector(model.weights(mf)) if(!is.null(weights) && !is.numeric(weights)) stop("'weights' must be a numeric vector") ## check weights and offset if( !is.null(weights) && any(weights < 0) )	stop("negative weights not allowed")
+
+mf <- match.call(expand.dots = FALSE) m <- match(c("formula", "data", "subset", "weights", "na.action", "etastart", "mustart", "offset"), names(mf), 0L) mf <- mf[c(1L, m)] mf$drop.unused.levels <- TRUE ## need stats:: for non-standard evaluation mf[[1L]] <- quote(stats::model.frame) mf <- eval(mf, parent.frame())
+
+mt <- attr(mf, "terms") # allow model.frame to have updated it
+
+
+X <- model.matrix(mt, mf, contrasts)
+
+
+
   
   # Easy setup for now.
   set.seed(1)
