83c2d3d8023abf4c5cc0a41e737779d01d1ae6e5
Tim Lucas <timcdlucas@gmail.com>
2022-01-11 16:05:27 +0000

    Fix small package issues

--- R/agouti_fit.R
+++ R/agouti_fit.R
@@ -18,14 +18,16 @@
 #' 
 #' @examples
 #' data(madagascar_malaria)
-#' agoutiGLM(case_rate ~ LSTmean, data = madagascar_malaria, ID = ID, 
-#'           weights = pop)
+# agoutiGLM(case_rate ~ LSTmean, data = madagascar_malaria,
+#           ID = madagascar_malaria$ID, weights = madagascar_malaria$pop,
+#           inner_link = 'identity', outer_link = 'identity',
+#           family = 'gaussian')
 
 
 agoutiGLM <- function(formula, data,
-                   ID, inner_link,
-                   outer_link,
-                   family, weights, ...){
+                      ID, inner_link = 'identity',
+                      outer_link = 'identity',
+                      family = gaussian(), weights, ...){
   
     
   # mostly stolen from glm as I want it to be consistent.
@@ -40,15 +42,15 @@ agoutiGLM <- function(formula, data,
   if(!is.null(weights) && !is.numeric(weights)) stop("'weights' must be a numeric vector") ## check weights and offset 
   if( !is.null(weights) && any(weights < 0) )	stop("negative weights not allowed")
   
-  mf <- model.frame(formula = formula, data = data,
+  mf <- stats::model.frame(formula = formula, data = data,
                     subset = NULL, weights = weights)
   
   
-  X <- model.matrix(formula, mf)
-  Y <- model.response(mf, "any") # e.g. factors are allowed ## avoid problems with 1D arrays, but keep names
+  X <- stats::model.matrix(formula, mf)
+  Y <- stats::model.response(mf, "any") # e.g. factors are allowed ## avoid problems with 1D arrays, but keep names
   
   if(family$family == 'gaussian'){
-    likelihood_function <- function(yhat, y, theta) dnorm(yhat, y, 3, log = TRUE)
+    likelihood_function <- function(yhat, y, theta) stats::dnorm(yhat, y, 3, log = TRUE)
   }
   if(inner_link == 'identity') inner <- function(x) x
   if(outer_link == 'identity') outer <- function(x) x
@@ -86,6 +88,7 @@ agoutiGLM <- function(formula, data,
 #' function such as dnorm.
 #' @param weights Which column to use as within group weights such as 
 #'   population or time.
+#' @importFrom rlang .data
 
 
 loglike <- function(beta, x, y, ID, inner,
@@ -99,8 +102,8 @@ loglike <- function(beta, x, y, ID, inner,
     dsum <-
         d %>%
         dplyr::group_by(ID) %>%
-        dplyr::summarise(summed = sum(summand),
-                         yhat = outer(summed))
+        dplyr::summarise(summed = sum(.data$summand),
+                         yhat = outer(.data$summed))
 
     ydf <- data.frame(y, ID) %>% 
              dplyr::distinct(y, ID)
@@ -109,7 +112,7 @@ loglike <- function(beta, x, y, ID, inner,
     dsum_join <-
         dsum %>%
             dplyr::left_join(ydf, by = c('ID' = 'ID')) %>%
-            dplyr::mutate(loglike = likelihood_function(yhat, y, theta))
+            dplyr::mutate(loglike = likelihood_function(.data$yhat, .data$y, .data$theta))
 
   return(dsum_join)
 }
