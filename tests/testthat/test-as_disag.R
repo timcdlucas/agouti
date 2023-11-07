06afea6b2ae064d4c7b0b3160b36e3adb8fe3b69
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-07 15:17:40 +0000

    some more tests for as_disag

--- tests/testthat/test-as_disag.R
+++ tests/testthat/test-as_disag.R
@@ -1,17 +1,49 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+## Testing as_disag function
+
 test_that("as_disag returns a dataframe in the correct format", {
 
   data("madagascar_malaria")
-  expect_equal(class(as_disag(madagascar_malaria, outcome="case_rate"))[1], "as_disag")
+  expect_is(as_disag(madagascar_malaria, outcome="case_rate"),"as_disag")
 
   data("stock_vector")
-  expect_equal(class(as_disag(stock_vector))[1], "as_disag")
+  expect_is(as_disag(stock_vector), "as_disag")
 
   data("mortality_temporal")
-  expect_equal(class(as_disag(mortality_temporal$Datetime, response_df=mortality_temporal))[1], "as_disag")
+  expect_is(as_disag(mortality_temporal$Datetime, response_df=mortality_temporal), "as_disag")
 
-  ## add examples for the other methods
+  ### expect true inherits class as_disag
+  ## these tests so far only consider three of the methods
+})
+
+test_that("as_disag returns a dataframe containing an ID column", {
+
+  data("madagascar_malaria")
+  expect_true("ID" %in% names(as_disag(madagascar_malaria, outcome="case_rate")))
+
+  data("stock_vector")
+  expect_true("ID" %in% names(as_disag(stock_vector)))
+
+  data("mortality_temporal")
+  expect_true("ID" %in% names(as_disag(mortality_temporal$Datetime, response_df=mortality_temporal)))
+
+})
+
+test_that("as_disag returns a dataframe where all rows with the same ID have the same outcome value", {
+
+  data("madagascar_malaria")
+  df <- as_disag(madagascar_malaria, outcome="case_rate") %>% dplyr::group_by(.data$ID)%>%
+    dplyr::mutate(unique_response=dplyr::n_distinct(case_rate))
+  expect_true(all(df$unique_response==1))
+
+  data("stock_vector")
+  df <- as_disag(stock_vector) %>% dplyr::group_by(.data$ID)%>%
+    dplyr::mutate(unique_response=dplyr::n_distinct(outcome))
+  expect_true(all(df$unique_response==1))
+
+  data("mortality_temporal")
+  df <- as_disag(mortality_temporal$Datetime, response_df=mortality_temporal) %>%
+    dplyr::group_by(.data$ID)%>%
+    dplyr::mutate(unique_response=dplyr::n_distinct(outcome))
+  expect_true(all(df$unique_response==1))
 
 })
