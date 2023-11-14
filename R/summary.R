c471f838efd6e54c41308d31653e2b34adcb3074
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-14 09:43:27 +0000

    Updates to summary function, should work with just one variable to summarise and can select which variables to summarise

--- R/summary.R
+++ R/summary.R
@@ -1,5 +1,4 @@
 
-
 #' Summarise a aggregated output dataset.
 #'
 #' @importFrom magrittr %>%
@@ -8,13 +7,16 @@
 #' @importFrom tidyr drop_na
 #' @param x A data frame suitable for use as aggregate output data.
 #' @param ID The column to use as the group ID column. Unquoted value.
+#' @param high_res A string or vector of strings specifiying a column or columns of high resolution variables to summarise.
+#' default is NULL in which case the function will summarise all variables with more unique values than ID
 #' @param removeNA default is false, is removeNA is not equal to false, NA's will be dropped
 #' @export
 #' @examples
 #' data(madagascar_malaria)
 #' agouti_summary(madagascar_malaria)
+#' agouti_summary(madagascar_malaria, high_res=c("pop","EVI"))
 
-agouti_summary <- function(x, ID = ID, removeNA = FALSE){
+agouti_summary <- function(x, ID = ID, high_res=NA, removeNA = FALSE){
 
   if(removeNA!=FALSE){
     x <- x %>% tidyr::drop_na()
@@ -29,33 +31,62 @@ agouti_summary <- function(x, ID = ID, removeNA = FALSE){
   min_j <- min(table(x[, ID_char, drop = TRUE]))
   median_j <- median(table(x[, ID_char, drop = TRUE]))
 
+  if(any(is.na(high_res))){
+    # find cols at aggregate level and remove
+    unique_vals <-
+      x %>%
+      sapply(function(col) length(unique(col)))
+    not_agg_level <- which(!(unique_vals == n_groups))
+  }else {
+
+    if(!all(high_res %in% names(x)))
+      stop("High resolution variables are not in data, please check spelling")
+
+    not_agg_level <- as.numeric(which(names(x) %in% high_res))
+  }
+
+  if(length(not_agg_level) == 1){
+
+    col_name <- names(x)[not_agg_level]
+    group_summary_table <-
+      x %>%
+      dplyr::group_by({{ID}}) %>%
+      dplyr::select(dplyr::any_of(not_agg_level)) %>%
+      dplyr::select_if(is.numeric) %>%
+      dplyr::summarise(across(c(all_of(col_name)), list(median=median,min=min,max=max))) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(- {{ID}}) %>%
+      dplyr::summarise_all(median) %>%
+      tidyr::pivot_longer(dplyr::everything()) %>%
+      dplyr::mutate(type = ifelse(grepl('median', .data$name), 'Median of medians', 'other')) %>%
+      dplyr::mutate(type = ifelse(grepl('min', .data$name), 'Median of mins', .data$type)) %>%
+      dplyr::mutate(type = ifelse(grepl('max', .data$name), 'Median of maxs', .data$type)) %>%
+      dplyr::mutate(name = gsub('_median|_min|_max', '', .data$name)) %>%
+      tidyr::pivot_wider()
+  }else{
 
 
-  # Find columns at aggregate level and remove.
-  unique_vals <-
-    x %>%
-    sapply(function(col) length(unique(col)))
-  not_agg_level <- which(!(unique_vals == n_groups))
-
-  group_summary_table <-
-    x %>%
-    dplyr::group_by({{ID}}) %>%
-    dplyr::select(dplyr::any_of(not_agg_level)) %>%
-    dplyr::select_if(is.numeric) %>%
-    dplyr::summarise_all(list(median = median, min = min, max = max)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(- {{ID}}) %>%
-    dplyr::summarise_all(median) %>%
-    tidyr::pivot_longer(dplyr::everything()) %>%
-    dplyr::mutate(type = ifelse(grepl('median', .data$name), 'Median of medians', 'other')) %>%
-    dplyr::mutate(type = ifelse(grepl('min', .data$name), 'Median of mins', .data$type)) %>%
-    dplyr::mutate(type = ifelse(grepl('max', .data$name), 'Median of maxs', .data$type)) %>%
-    dplyr::mutate(name = gsub('_median|_min|_max', '', .data$name)) %>%
-    tidyr::pivot_wider()
+    group_summary_table <-
+      x %>%
+      dplyr::group_by(ID) %>%
+      dplyr::select(dplyr::any_of(not_agg_level)) %>%
+      dplyr::select_if(is.numeric) %>%
+      dplyr::summarise_all(list(median = median, min = min, max = max)) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(- {{ID}}) %>%
+      dplyr::summarise_all(median) %>%
+      tidyr::pivot_longer(dplyr::everything()) %>%
+      dplyr::mutate(type = ifelse(grepl('median', .data$name), 'Median of medians', 'other')) %>%
+      dplyr::mutate(type = ifelse(grepl('min', .data$name), 'Median of mins', .data$type)) %>%
+      dplyr::mutate(type = ifelse(grepl('max', .data$name), 'Median of maxs', .data$type)) %>%
+      dplyr::mutate(name = gsub('_median|_min|_max', '', .data$name)) %>%
+      tidyr::pivot_wider()
+  }
 
   print(paste("Aggregate outputs table with ID column:", ID_char))
   print(paste(n_rows, 'total rows in', n_groups, 'groups.'))
   print(paste0('Min group size: ', min_j, '. Median group size: ', median_j, '. Max group size: ', max_j, '.'))
+
   print(group_summary_table)
 
   return(invisible(group_summary_table))
