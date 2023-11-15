7e36f6a7b4129c9a7cd681b6d7ac62f342e07dfa
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-15 10:18:00 +0000

    updating tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,9 +1,11 @@
 test_that('Heat_hist makes a plot of class gg', {
 
   data("madagascar_malaria")
-  pp <- heat_hist(madagascar_malaria$EVI, madagascar_malaria$ID)
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  pp <- heat_hist(disag_data$EVI, disag_data$ID)
   data("stock_data")
-  p <- heat_hist(stock_data$lagged_growth, stock_data$ID)
+  disag_ts <- as_disag(stock_data, outcome="growth")
+  p <- heat_hist(disag_ts$lagged_growth, disag_ts$ID)
   expect_true(inherits(pp, "gg"))
   expect_true(inherits(p, "gg"))
 })
@@ -11,9 +13,11 @@ test_that('Heat_hist makes a plot of class gg', {
 test_that('group summary plot makes a plot of class gg', {
 
   data("madagascar_malaria")
-  pp <- group_summary_plot(case_rate ~ LSTmean, data = madagascar_malaria, ID = ID, weights = pop)
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  pp <- group_summary_plot(case_rate ~ LSTmean, data = disag_data, ID = ID, weights = pop)
   data("stock_data")
-  p <- group_summary_plot(growth ~ lagged_growth, data = stock_data, ID = ID, weights=1)
+  disag_ts <- as_disag(stock_data, outcome="growth")
+  p <- group_summary_plot(growth ~ lagged_growth, data = disag_ts, ID = ID, weights=1)
   expect_true(inherits(pp, "gg"))
   expect_true(inherits(p, "gg"))
 })
@@ -21,9 +25,11 @@ test_that('group summary plot makes a plot of class gg', {
 test_that('threshold small multiple plot makes a plot of class gg', {
 
   data("madagascar_malaria")
-  pp <- thresh_sm(case_rate ~ LSTmean, data = madagascar_malaria, ID = ID, weights = pop)
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  pp <- thresh_sm(case_rate ~ LSTmean, data = disag_data, ID = ID, weights = pop)
   data("stock_data")
-  p <- thresh_sm(growth ~ lagged_growth, data = stock_data, ID = ID)
+  disag_ts <- as_disag(stock_data, outcome="growth")
+  p <- thresh_sm(growth ~ lagged_growth, data = disag_ts, ID = ID)
   expect_true(inherits(pp, "gg"))
   expect_true(inherits(p, "gg"))
 })
@@ -31,9 +37,13 @@ test_that('threshold small multiple plot makes a plot of class gg', {
 test_that('Link plot makes a plot of class gg', {
 
   data("madagascar_malaria")
-  pp <- link_plot(case_rate ~ LSTmean, data = madagascar_malaria, ID = ID, weights = pop)
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  pp <- link_plot(case_rate ~ LSTmean, data = disag_data, ID = ID, weights = pop)
   data("stock_data")
-  p <- link_plot(growth ~ lagged_growth, data = stock_data, ID = ID, weights = 1)
+  disag_ts <- as_disag(stock_data, outcome="growth")
+  p <- link_plot(growth ~ lagged_growth, data = disag_ts, ID = ID, weights = 1)
   expect_true(inherits(pp, "gg"))
   expect_true(inherits(p, "gg"))
 })
+
+
--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,23 +1,22 @@
 test_that("Summary fails if names of variables mis-spelt", {
   data("madagascar_malaria")
-  expect_error(agouti_summary(madagascar_malaria, high_res="POP"), "check spelling")
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  expect_error(agouti_summary(disag_data, high_res="POP"), "check spelling")
 
 })
 
 
 test_that("Summary fails if named ID variable is not present", {
   data("madagascar_malaria")
-  expect_error(agouti_summary(madagascar_malaria, ID="id"), "Data does not contain the variable")
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  expect_error(agouti_summary(disag_data, ID="id"), "Data does not contain the variable")
 })
 
 
 test_that("Summary creates a table in the correct format", {
   data("madagascar_malaria")
-  expect_true(nrow(agouti_summary(madagascar_malaria, high_res="pop"))==3)
-  expect_true(ncol(agouti_summary(madagascar_malaria, high_res="pop"))>=1)
+  disag_data <- as_disag(madagascar_malaria, outcome="case_rate")
+  expect_true(nrow(agouti_summary(disag_data, high_res="pop"))==3)
+  expect_true(ncol(agouti_summary(disag_data, high_res="pop"))>=1)
 
-  data("mortality_temporal")
-  disag_data <- as_disag(mortality_temporal$Datetime, response_df=mortality_temporal,time_group="%Y%m%d",outcome="Death")
-  expect_true(nrow(agouti_summary(disag_data))==3)
-  expect_true(ncol(agouti_summary(disag_data))>=1)
 })
