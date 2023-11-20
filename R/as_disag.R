ef7d9c4f13e8258174350ed297b460220daece08
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-20 13:25:12 +0000

    switch dependencies for change from parallelExtract to prepare data

--- R/as_disag.R
+++ R/as_disag.R
@@ -1,8 +1,9 @@
 
-#' as_dsiag Generic function
+#' as_disag Generic function
 #'
 #' @param data A dataframe containing at least an outcome variable and ID variable named ID
 #' @param ... any other arguments
+#' @method
 #' @export
 as_disag <- function(data,...){
   UseMethod("as_disag")
@@ -76,87 +77,195 @@ as_disag.data.frame <- function (data,data2=NULL,outcome,...){
 }
 
 #' Create a data.frame suitable for disaggregation regression modelling from
-#' a spatial polygons dataframe.
-#' @importFrom parallel detectCores makeCluster stopCluster
-#' @importFrom doParallel registerDoParallel
-#' @importFrom raster extract
-#' @importFrom foreach foreach
-#' @param data A spatial polygons data frame with a column named ID
-#' @param rstack A raster stack of rasters to be summarised at ID level
-#' @param response_df a dataframe containing the response data and an ID variable
+#' a sf object.
+#' @importFrom terra extract match
+#' @param data An object of class sf containing polygons that covariates should be aggregated to and including the response variable and ID
+#' @param rstack A raster stack of class SpatRaster to be summarised at ID level
+#' @param response_var Name of the response variable found in data
 #' @param ... any other arguments
-#' @return \code{as_disag.SpatialPolygonsDataFrame} returns a dataframe that can be used with all agouti functions.
-#' @method as_disag SpatialPolygonsDataFrame
+#' @return \code{as_disag.sf} returns a dataframe that can be used with all agouti functions.
+#' @method as_disag sf
 #' @export
 #' @examples
 #' \dontrun{
-#' disag_data <- as_disag(data=shapefile, rstack=rstack, response=wnv)
+#' disag_data <- as_disag(data=shapefile, rstack=rstack, response_var=Human.cases)
 #' }
-as_disag.SpatialPolygonsDataFrame <- function (data, rstack, response_df,...){
+as_disag.sf <- function (data, rstack, response_var="response",...){
 
   ## first need some checks on each of the objects
-  if(!("ID" %in% names(data@data)))
+  if(!("ID" %in% names(data)))
     stop("ID varible not found in data, please check")
-  if(!("ID" %in% names(response_df)))
+  if(!(response_var %in% names(data)))
     stop("ID varible not found in response data, please check")
 
-  if(!(all(unique(response_df$ID) %in% unique(data@data$ID))))
-    stop("Some IDs in response data are not in spatial polygons dataframe")
+  prepare_data <- function(polygon_shapefile,
+                           covariate_rasters,
+                           aggregation_raster = NULL,
+                           id_var = 'area_id',
+                           response_var = 'response',
+                           sample_size_var = NULL,
+                           # mesh.args = NULL,
+                           na.action = FALSE) {
+    # makeMesh = TRUE,
+    #ncores = NULL) {
 
-  ## use the extract function from disagreggation package to extract data from raster stack at polygon level
-  nCores <- parallel::detectCores()-1
-  cl <- parallel::makeCluster(nCores)
-  doParallel::registerDoParallel(cl)
-  print("Extracting data from raster stack, this may take a while...")
-  parallelExtract <- function(raster, shape, fun = mean, id = 'OBJECTID',  ...){
+    # if (!missing("ncores"))
+    #   warning("The ncores argument has been deprecated")
 
-    if (!requireNamespace("foreach", quietly = TRUE)) {
-      stop("foreach needed for this function to work. Please install it.",
-           call. = FALSE)
+    stopifnot(inherits(polygon_shapefile, 'sf'))
+    stopifnot(inherits(covariate_rasters, 'SpatRaster'))
+    if(!is.null(aggregation_raster)) stopifnot(inherits(aggregation_raster, 'SpatRaster'))
+    stopifnot(inherits(id_var, 'character'))
+    stopifnot(inherits(response_var, 'character'))
+    # if(!is.null(mesh.args)) stopifnot(inherits(mesh.args, 'list'))
+
+    # Check for NAs in response data
+    na_rows <- is.na(polygon_shapefile[, response_var, drop = TRUE])
+    if(sum(na_rows) != 0) {
+      if(na.action) {
+        polygon_shapefile <- polygon_shapefile[!na_rows, ]
+      } else {
+        stop('There are NAs in the response data. Please deal with these, or set na.action = TRUE')
+      }
     }
+    getPolygonData <- function(shape, id_var = 'area_id', response_var = 'response', sample_size_var = NULL) {
 
-    shape@data[, id] <- as.character(shape@data[, id])
-
-    i <- NULL
-    # Run extract in parallel.
-    values <- foreach::foreach(i = seq_along(shape)) %dopar% {
-      raster::extract(raster, shape[i, ], fun = fun, na.rm = TRUE, cellnumbers = TRUE, ...)
-    }
-    if(!is.null(fun)){
-      # If a summary function was given, just bind everything together and add ID column
-      df <- data.frame(do.call(rbind, values))
-      if(inherits(shape, 'SpatialPolygonsDataFrame')){
-        df <- cbind(ID = as.data.frame(shape)[, id], df)
-      } else{
-        df <- cbind(ID = names(shape), df)
-        id <- 'id'
+      if(is.null(sample_size_var)) {
+        polygon_df <- shape[, c(id_var, response_var), drop = TRUE]
+        polygon_df$N <- rep(NA, nrow(polygon_df))
+      } else {
+        polygon_df <- shape[, c(id_var, response_var, sample_size_var), drop = TRUE]
       }
 
-      names(df) <- c(id, names(raster))
+      names(polygon_df) <- c('area_id', 'response', 'N')
 
-      return(df)
-    } else {
-      # If no summary was given we get a list of length n.shapes
-      #   each entry in the list is a dataframe with n.covariates columns
-      #   Want to make covariates columns, rbind shapes, and add shape and cell id columns.
-
-      # list of vectors, one for each covariate
-      values_id <- lapply(seq_along(values), function(x) data.frame(shape@data[, id][x], values[[x]][[1]]))
-
-
-      df <- do.call(rbind, values_id)
-      names(df) <- c(id, 'cellid', names(raster))
-
-      return(df)
+      return(polygon_df)
     }
 
-  }
-  result <- parallelExtract(rstack, data, id="ID",fun=NULL)
-  parallel::stopCluster(cl)
+    polygon_data <- getPolygonData(polygon_shapefile, id_var, response_var, sample_size_var)
 
-  response_df <- dplyr::left_join(response_df, result, by = "ID")
-  class(response_df) <- append("as_disag", class(response_df))
-  return(response_df)
+
+    # Save raster layer names so we can reassign it to make sure names don't change.
+    cov_names <- names(covariate_rasters)
+
+    # If no aggregation raster is given, use a 'unity' raster
+    if(is.null(aggregation_raster)) {
+      aggregation_raster <- covariate_rasters[[1]]
+      terra::values(aggregation_raster) <- rep(1, terra::ncell(aggregation_raster))
+    }
+    names(aggregation_raster) <- 'aggregation_raster'
+
+
+    covariate_rasters <- c(covariate_rasters, aggregation_raster)
+    covariate_data <- terra::extract(covariate_rasters, terra::vect(polygon_shapefile), cells=TRUE, na.rm=TRUE, ID=TRUE)
+    #merge to transfer area_id and then tidy up
+    polygon_data$area_n <- 1:nrow(polygon_data)
+    covariate_data <- merge(covariate_data, polygon_data, by.x = "ID", by.y = "area_n")
+    covariate_data <- covariate_data[ , !(names(covariate_data) %in% c("ID", "response", "N"))]
+    colnames(covariate_data )[colnames(covariate_data ) == "area_id"] <- id_var
+    polygon_data <- polygon_data[ , !(names(polygon_data) %in% c("area_n"))]
+
+    # Remove the aggregation raster
+    cov_filter <- !(names(covariate_data) %in% c('aggregation_raster'))
+    covariate_rasters <- covariate_rasters[[cov_filter]]
+    names(covariate_rasters) <- cov_names
+
+    agg_filter <- names(covariate_data) %in% c('aggregation_raster')
+    aggregation_pixels <- as.numeric(covariate_data[ , agg_filter])
+    covariate_data <- covariate_data[, !agg_filter]
+
+    # Check for NAs in population data
+    if(sum(is.na(aggregation_pixels)) != 0) {
+      if(na.action) {
+        aggregation_pixels[is.na(aggregation_pixels)] <- 0
+      } else {
+        stop('There are NAs in the aggregation rasters within polygons. Please deal with these, or set na.action = TRUE')
+      }
+    }
+
+    # Check for NAs in covariate data
+    if(sum(is.na(covariate_data)) != 0) {
+      if(na.action) {
+        cov_filter <- !(names(covariate_data) %in% c(id_var,'cell'))
+        covariate_data[ , cov_filter] <- sapply(covariate_data[ , cov_filter], function(x) { x[is.na(x)] <- stats::median(x, na.rm = T); return(x) })
+      } else {
+        stop('There are NAs in the covariate rasters within polygons. Please deal with these, or set na.action = TRUE')
+      }
+    }
+    # extractCoordsForMesh <- function(cov_rasters, selectIds = NULL) {
+    #
+    #   stopifnot(inherits(cov_rasters, 'SpatRaster'))
+    #   if(!is.null(selectIds)) stopifnot(inherits(selectIds, 'numeric'))
+    #
+    #   points_raster <- cov_rasters[[1]]
+    #   points_raster[is.na(terra::values(points_raster, mat = FALSE))] <- -9999
+    #   raster_pts <- terra::as.points(points_raster)
+    #   coords <- terra::crds(raster_pts)
+    #
+    #   # If specified, only retain certain pixel ids
+    #   if(!is.null(selectIds)) {
+    #     coords <- coords[selectIds, ]
+    #   }
+    #
+    #   return(coords)
+    #
+    # }
+    # coordsForFit <- extractCoordsForMesh(covariate_rasters, selectIds = covariate_data$cell)
+    #
+    # coordsForPrediction <- extractCoordsForMesh(covariate_rasters)
+    getStartendindex <- function(covariates, polygon_data, id_var = 'area_id') {
+
+      stopifnot(ncol(polygon_data) == 3)
+      stopifnot(ncol(covariates) >= 2)
+      stopifnot(nrow(covariates) > nrow(polygon_data))
+      stopifnot(sum(polygon_data$area_id %in% covariates[, id_var]) == nrow(polygon_data))
+
+      # Create  startendindex matrix
+      # This defines which pixels in the matrix are associated with which polygon.
+      startendindex <- lapply(unique(covariates[, id_var]), function(x) range(which(covariates[, id_var] == x)))
+
+      startendindex <- do.call(rbind, startendindex)
+
+      whichindices <- terra::match(polygon_data$area_id, unique(covariates[, id_var]))
+
+      # c++ is zero indexed.
+      startendindex <- startendindex[whichindices, ] - 1L
+
+      return(startendindex)
+    }
+    startendindex <- getStartendindex(covariate_data, polygon_data, id_var = id_var)
+
+    # if(makeMesh) {
+    #   mesh <- build_mesh(polygon_shapefile, mesh.args)
+    # } else {
+    #   mesh <- NULL
+    #   message("A mesh is not being built. You will not be able to run a spatial model without a mesh.")
+    # }
+
+    disag_data <- list(polygon_shapefile = polygon_shapefile,
+                       shapefile_names = list(id_var = id_var, response_var = response_var),
+                       covariate_rasters = covariate_rasters,
+                       polygon_data = polygon_data,
+                       covariate_data = covariate_data,
+                       aggregation_pixels = aggregation_pixels,
+                       #coordsForFit = coordsForFit,
+                       #coordsForPrediction = coordsForPrediction,
+                       startendindex = startendindex)
+    #mesh = mesh)
+
+    disag_data <- disag_data$covariate_data
+    disag_data <- disag_data %>% dplyr::left_join(polygon_shapefile, by = "ID")
+
+    return(disag_data)
+
+  }
+
+  df_summ <- prepare_data(polygon_shapefile =data, covariate_rasters = rstack, id_var="ID",
+                        response_var=response_var,na.action=TRUE)
+
+
+  class(df_summ) <- append("as_disag", class(df_summ))
+  return(df_summ)
 
 
 }
