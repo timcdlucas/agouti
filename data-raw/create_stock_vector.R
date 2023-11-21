4a09f2f91442bc5926e06c615dcb2eb812c8abff
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-21 15:29:28 +0000

    Adding raw stock vector dataset

--- data-raw/create_stock_vector.R
+++ data-raw/create_stock_vector.R
@@ -1,3 +1,29 @@
-## code to prepare `stock_vector` dataset goes here
+## code to prepare `stock_vector` dataset
 
-usethis::use_data(stock_vector, overwrite = TRUE)
+library(tidyquant)
+library(dplyr)
+options("getSymbols.warning4.0"=FALSE)
+options("getSymbols.yahoo.warning"=FALSE)
+
+prices <- tq_get(symbols$symbol[400:450],
+                 from = "2002-01-01",
+                 to = "2020-03-01",
+                 get = "stock.prices")
+
+ggplot(prices, aes(x = date, y = open, colour = symbol, group = symbol)) +
+  geom_line()
+
+
+ggplot(prices %>% dplyr::filter(symbol == 'AMCR'), aes(x = date, y = open, colour = symbol, group = symbol)) +
+  geom_line()
+
+just_growth<-
+  prices %>%
+  filter(symbol == 'VTRS') %>%
+  mutate(growth = close / lag(close) - 1) %>%
+  select(growth)
+
+just_growth <- na.omit(just_growth) %>% pull(growth)
+
+stock_vector <- just_growth
+usethis::use_data(stock_vector,overwrite=TRUE)
