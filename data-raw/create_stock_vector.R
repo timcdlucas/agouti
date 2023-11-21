330fa24b7966b8e3efbc861c4e30526e9f223a8e
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-21 16:32:42 +0000

    think I made a mistake in copying and pasting the stock vector generation code

--- data-raw/create_stock_vector.R
+++ data-raw/create_stock_vector.R
@@ -2,8 +2,53 @@
 
 library(tidyquant)
 library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(broom)
+
 options("getSymbols.warning4.0"=FALSE)
 options("getSymbols.yahoo.warning"=FALSE)
+# Downloading Apple price using quantmod
+
+
+symbols <- tq_index("SP500")
+
+
+
+prices <- tq_get(symbols$symbol,
+                 from = "2015-01-01",
+                 to = "2020-03-01",
+                 get = "stock.prices")
+
+
+prices <-
+  prices %>%
+  filter(date > as.Date('2017-01-01'))
+
+prices2 <-
+  prices %>%
+  group_by(symbol) %>%
+  mutate(growth = close / lag(close) - 1)
+
+
+
+library(tidyquant)
+library(dplyr)
+options("getSymbols.warning4.0"=FALSE)
+options("getSymbols.yahoo.warning"=FALSE)
+
+prices <- tq_get(symbols$symbol[1],
+                 from = "2017-01-01",
+                 to = "2020-03-01",
+                 get = "stock.prices")
+
+
+prices2 <-
+  prices %>%
+  mutate(growth = close / lag(close) - 1)
+
+
+
 
 prices <- tq_get(symbols$symbol[400:450],
                  from = "2002-01-01",
@@ -17,6 +62,7 @@ ggplot(prices, aes(x = date, y = open, colour = symbol, group = symbol)) +
 ggplot(prices %>% dplyr::filter(symbol == 'AMCR'), aes(x = date, y = open, colour = symbol, group = symbol)) +
   geom_line()
 
+
 just_growth<-
   prices %>%
   filter(symbol == 'VTRS') %>%
@@ -25,5 +71,7 @@ just_growth<-
 
 just_growth <- na.omit(just_growth) %>% pull(growth)
 
+
 stock_vector <- just_growth
+
 usethis::use_data(stock_vector)
