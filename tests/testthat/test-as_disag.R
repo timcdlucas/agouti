04d7ae3663d1b0f79af89c96acda28a84447650a
Suzanne Keddie <suzanne313.sk@gmail.com>
2023-11-22 13:02:27 +0000

    tests

--- tests/testthat/test-as_disag.R
+++ tests/testthat/test-as_disag.R
@@ -8,13 +8,6 @@ test_that("as_disag returns a dataframe in the correct format", {
   data("stock_vector")
   expect_true(inherits(as_disag(stock_vector), "as_disag"))
 
-  ## sf example
-  polygons <- sf::st_as_sf(NYleukemia$spatial.polygon)
-  df <- cbind(polygons, NYleukemia$data)
-  names(df)[1] <-"ID"
-  covariate <- terra::rast("vignettes/annual_mean_temp_newyork.tif")
-  expect_true(inherits(as_disag(data=df,rstack=covariate, response_var="cases")))
-
   ## these tests so far only consider three of the methods
 })
 
@@ -25,13 +18,6 @@ test_that("as_disag returns a dataframe containing an ID column", {
 
   data("stock_vector")
   expect_true("ID" %in% names(as_disag(stock_vector)))
-
-  ## sf example
-  polygons <- sf::st_as_sf(NYleukemia$spatial.polygon)
-  df <- cbind(polygons, NYleukemia$data)
-  names(df)[1] <-"ID"
-  covariate <- terra::rast("vignettes/annual_mean_temp_newyork.tif")
-  expect_true("ID" %in% names(as_disag(data=df,rstack=covariate, response_var="cases")))
 
 })
 
@@ -46,15 +32,6 @@ test_that("as_disag returns a dataframe where all rows with the same ID have the
   df <- as_disag(stock_vector) %>% dplyr::group_by(.data$ID)%>%
     dplyr::mutate(unique_response=dplyr::n_distinct(outcome))
   expect_true(all(df$unique_response==1))
-
-  ## sf example
-  polygons <- sf::st_as_sf(NYleukemia$spatial.polygon)
-  df <- cbind(polygons, NYleukemia$data)
-  names(df)[1] <-"ID"
-  covariate <- terra::rast("vignettes/annual_mean_temp_newyork.tif")
-  df2 <- as_disag(data=df,rstack=covariate, response_var="cases") %>% dplyr::group_by(.data$ID)%>%
-    dplyr::mutate(unique_response=dplyr::n_distinct(cases))
-  expect_true(all(df2$unique_response==1))
 
 
 })
